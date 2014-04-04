2ec8e1f687addde95299058933fb9020a085cd4d
Mike Reposa <mreposa@cra.com>
2014-04-04 15:59:26 -0400

    Updates to SBT build file and .gitignore

--- project/Build.scala
+++ project/Build.scala
@@ -4,11 +4,33 @@ import Keys._
 object FigaroBuild extends Build {
 
   override val settings = super.settings ++ Seq(
-    organization := "figaro",
+    organization := "com.cra.figaro",
+    description := "Figaro: a language for probablistic programming",
     version := "2.2.0.0",
     scalaVersion := "2.10.1",
     crossPaths := false,
-    publishMavenStyle := true)
+    publishMavenStyle := true,
+    pomExtra :=
+	<url>http://www.github.com/p2t2/figaro</url>
+	<developers>
+	  <developer>
+	    <name>Avrom J. Pfeffer</name>
+	    <email>apfeffer@cra.com</email>
+	    <organization>Charles River Analytics, Inc.</organization>
+	    <organizationUrl>http://www.cra.com</organizationUrl>
+	  </developer>
+	</developers>
+	<licenses>
+	  <license>
+	    <name>Figaro License</name>
+	    <url>https://github.com/p2t2/figaro/blob/master/LICENSE</url>
+	  </license>
+	</licenses>
+	<scm>
+	  <connection>scm:git:git@github.com:p2t2/figaro.git</connection>
+	  <developerConnection>scm:git:git@github.com:p2t2/figaro.git</developerConnection>
+	  <url>git@github.com:p2t2/figaro.git</url>
+	</scm>)
 
   lazy val root = Project("root", file("."))
     .settings(publishLocal := {})
@@ -33,5 +55,4 @@ object FigaroBuild extends Build {
   
   lazy val examples = Project("figaro-examples", file("FigaroExamples"))
     .dependsOn(figaro)
-
 }
