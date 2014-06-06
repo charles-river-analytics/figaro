a1498a4558c1bb881cc50960bce50f723c9c24c0
Mike Reposa <mreposa@cra.com>
2014-06-06 14:34:19 -0400

    Updates to start new branch

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object FigaroBuild extends Build {
   override val settings = super.settings ++ Seq(
     organization := "com.cra.figaro",
     description := "Figaro: a language for probablistic programming",
-    version := "2.2.0.0",
+    version := "2.2.1.0",
     scalaVersion := "2.10.4",
     //scalaVersion := "2.11.1",
     crossScalaVersions := Seq("2.10.4", "2.11.1"),
