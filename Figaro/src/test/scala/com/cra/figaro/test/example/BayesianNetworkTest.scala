4b14b96d039dd70c656d3ff77962221b229faa60
Glenn Takata <gtakata@cra.com>
2014-09-15 10:06:33 -0400

    Make item specific tests rather than generic

--- Figaro/src/test/scala/com/cra/figaro/test/example/BayesianNetworkTest.scala
+++ Figaro/src/test/scala/com/cra/figaro/test/example/BayesianNetworkTest.scala
@@ -16,6 +16,7 @@ package com.cra.figaro.test.example
 import org.scalatest.Matchers
 import org.scalatest.WordSpec
 import com.cra.figaro.algorithm.factored._
+import beliefpropagation.BeliefPropagation
 import com.cra.figaro.library.compound._
 import com.cra.figaro.language._
 import com.cra.figaro.algorithm._
@@ -27,25 +28,34 @@ import com.cra.figaro.test.tags.NonDeterministic
 class BayesianNetworkTest extends WordSpec with Matchers {
   "A simple Bayesian network" should {
     "produce the correct probability under variable elimination" taggedAs (Example, NonDeterministic) in {
-      test((e1: Element[Boolean], e2: Element[Boolean]) => VariableElimination(e1, e2))
+      val model = new Model
+      VariableElimination.probability(model.burglary, true) should be(model.pBurglary +- 0.01)
+    }
+
+    "produce the correct probability under belief propagation" taggedAs (Example, NonDeterministic) in {
+      val model = new Model
+      BeliefPropagation.probability(model.burglary, true) should be(model.pBurglary +- 0.01)
     }
 
     "produce the correct probability under importance sampling" taggedAs (Example, NonDeterministic) in {
-      test((e1: Element[Boolean], e2: Element[Boolean]) => Importance(10000, e1, e2))
+      val model = new Model
+      Importance.probability(model.burglary, true) should be(model.pBurglary +- 0.01)
     }
-    
+
     "produce the correct probability under Metropolis-Hastings without burn-in or interval" taggedAs (Example, NonDeterministic) in {
-      test((e1: Element[Boolean], e2: Element[Boolean]) =>
-        MetropolisHastings(2000000, ProposalScheme.default, e1, e2))
+      val model = new Model
+      MetropolisHastings.probability(model.burglary, true, 2000000) should be(model.pBurglary +- 0.01)
     }
-    
+
     "produce the correct probability under Metropolis-Hastings with burn-in and interval" taggedAs (Example, NonDeterministic) in {
-      test((e1: Element[Boolean], e2: Element[Boolean]) =>
-        MetropolisHastings(200000, ProposalScheme.default, 800, 10, e1, e2))
+      val model = new Model
+      val alg = MetropolisHastings(200000, ProposalScheme.default, 800, 10, model.burglary)
+      alg.start()
+      alg.probability(model.burglary, true) should be(model.pBurglary +- 0.01)
     }
   }
 
-  def test(algorithmCreator: (Element[Boolean], Element[Boolean]) => ProbQueryAlgorithm): Unit = {
+  class Model {
     Universe.createNew()
     val burglaryProb = 0.1
     val earthquakeProb = 0.001
@@ -74,11 +84,5 @@ class BayesianNetworkTest extends WordSpec with Matchers {
     val qBurglary = burglaryProb * pJohnGivenBurglary
     val qNotBurglary = (1 - burglaryProb) * pJohnGivenNotBurglary
     val pBurglary = qBurglary / (qBurglary + qNotBurglary)
-
-    val alg = algorithmCreator(burglary, earthquake)
-    alg.start()
-    alg.stop()
-    alg.probability(burglary, true) should be(pBurglary +- 0.01)
-    alg.kill()
   }
 }
