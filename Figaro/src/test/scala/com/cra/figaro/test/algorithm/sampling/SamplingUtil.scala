2ab5a05242dd81062e0e81d4a5382f8ae88a3d4f
Mike Reposa <mreposa@cra.com>
2014-12-17 10:30:11 -0500

    Added missing file headers for tests

--- Figaro/src/test/scala/com/cra/figaro/test/algorithm/sampling/SamplingUtil.scala
+++ Figaro/src/test/scala/com/cra/figaro/test/algorithm/sampling/SamplingUtil.scala
@@ -1,3 +1,16 @@
+/*
+ * SamplingUtil.scala
+ * Utility functions supporting tests.
+ * 
+ * Created By:      Avi Pfeffer (apfeffer@cra.com)
+ * Creation Date:   Oct 6, 2014
+ * 
+ * Copyright 2014 Avrom J. Pfeffer and Charles River Analytics, Inc.
+ * See http://www.cra.com or email figaro@cra.com for information.
+ * 
+ * See http://www.github.com/p2t2/figaro for a copy of the software license.
+ */
+
 package com.cra.figaro.test.algorithm.sampling
 
 object SamplingUtil {
