1e258ac270da330c511560384920027ce98cd16a
Mike Reposa <mreposa@cra.com>
2015-03-20 13:19:43 -0400

    Initial ND test classes

--- Figaro/src/test/scala/com/cra/figaro/ndtest/NDTest.scala
+++ Figaro/src/test/scala/com/cra/figaro/ndtest/NDTest.scala
@@ -1,49 +1,37 @@
 /*
- * Importance.scala
- * Importance sampler.
+ * NDTest.scala 
+ * Runs non-deterministic tests
  * 
- * Created By:      Avi Pfeffer (apfeffer@cra.com)
- * Creation Date:   Jan 1, 2009
+ * Created By:      Michael Reposa (mreposa@cra.com) and Glenn Takata (gtakata@cra.com)
+ * Creation Date:   Mar 17, 2015
  * 
  * Copyright 2013 Avrom J. Pfeffer and Charles River Analytics, Inc.
  * See http://www.cra.com or email figaro@cra.com for information.
  * 
  * See http://www.github.com/p2t2/figaro for a copy of the software license.
  */
+
 package com.cra.figaro.ndtest
 
-/**
- * @author Glenn Takata Mar 5, 2015
- *
- */
-trait Result {
-  def update(value: Double)
-  
-  def report
-}
+import org.scalatest.WordSpec
+import org.scalatest.Matchers
+import org.scalatest.exceptions.TestFailedException
+import com.cra.figaro.language._
+import com.cra.figaro.algorithm._
 
-class NDTest {
-  var results: Map[String, Result] = Map()
-  
-  def run(n: Int) {
-    (0 to n).foreach(_ => test)
-    
+class NDTest extends WordSpec with Matchers
+{
+  var results: Map[String, NDTestResult] = Map()
+
+  final def run(n: Int)
+  {
+    (0 until n).foreach(_ => oneTest)
+
     for (result <- results.values)
-      result.report
-  }
-  
-  def test {
-//    val result1 = results.get("probResult")
-//    result1.update(newValue)
-  }
-  
-  class MeanResult(name: String, target: Double) extends Result {
-    var statistic: Double = 0
-    
-    def update(value: Double) {
-      statistic += value
+    {
+      result.check should be (true)
     }
-    
-    def report {}
   }
+  
+  def oneTest {}
 }
