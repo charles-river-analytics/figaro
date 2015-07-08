c46c89b570a368008bab89e7e90b383119c5f2cf
Mike Reposa <mreposa@cra.com>
2015-07-08 14:07:28 -0400

    More ScalaDoc fixes

--- Figaro/src/main/scala/com/cra/figaro/library/cache/PermanentCache.scala
+++ Figaro/src/main/scala/com/cra/figaro/library/cache/PermanentCache.scala
@@ -1,3 +1,15 @@
+/*
+ * PermanentCache.scala
+ * Only caches permanent result elements in chain.
+ *
+ * Created By:      Avi Pfeffer (apfeffer@cra.com)
+ * Creation Date:   Jan 1, 2009
+ *
+ * Copyright 2013 Avrom J. Pfeffer and Charles River Analytics, Inc.
+ * See http://www.cra.com or email figaro@cra.com for information.
+ *
+ * See http://www.github.com/p2t2/figaro for a copy of the software license.
+ */
 package com.cra.figaro.library.cache
 
 import com.cra.figaro.language._
