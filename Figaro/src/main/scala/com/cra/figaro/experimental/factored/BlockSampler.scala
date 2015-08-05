ff87ecdf78c4047408730e4ec77113c13d871724
wkretschmer <kretsch@mit.edu>
2015-08-05 16:56:41 -0400

    Block sampling improvements
    Block sampling defaults to the new factor product operation because it
    is more efficient on standard Figaro models. The addition of a priority
    queue to the factor product computation further improves efficiency by
    avoiding large intermediate factors even when the final product is
    sparse.

--- Figaro/src/main/scala/com/cra/figaro/experimental/factored/BlockSampler.scala
+++ Figaro/src/main/scala/com/cra/figaro/experimental/factored/BlockSampler.scala
@@ -17,63 +17,54 @@ import com.cra.figaro.algorithm.factored.factors._
 import com.cra.figaro.algorithm.lazyfactored.{Extended, Regular}
 import com.cra.figaro.util._
 import scala.annotation.tailrec
-import scala.collection.mutable.{LinkedHashMap, Map}
+import scala.collection.mutable.{LinkedHashMap, Map => MutableMap, PriorityQueue}
 
 object BlockSampler {
   /**
-   * The default block sampler, which caches an unlimited number of factors
+   * Caches 1000 factors and uses a VE-like procedure to compute joint factors
    */
-  def default = (blockInfo: Gibbs.BlockInfo) => new SimpleBlockSampler(blockInfo) with Cached
+  def default = (blockInfo: Gibbs.BlockInfo) => new SimpleBlockSampler(blockInfo) with Cached with FactorProduct
 
   /**
-   * The continuous block sampler, which uses a mixture of Gaussians approach when there exist continuous elements that are results of Chains
+   * Like BlockSampler.default, but also uses Gaussian weighting scheme when there exist Element[Double]s that are results of Chains
    */
-  def continuous = (blockInfo: Gibbs.BlockInfo) => new SimpleBlockSampler(blockInfo) with Cached with GaussianWeight
+  def continuous = (blockInfo: Gibbs.BlockInfo) => new SimpleBlockSampler(blockInfo) with Cached with GaussianWeight with FactorProduct
 
   /**
-   * The factor product block sampler, which uses a VE-like procedure to compute joint factors
-   * Works well on large but sparse blocks
+   * Computes the joint distribution over the block by iterating through every combination of assignments of variables
+   * Takes time exponential in the size of the block, and is only recommended for blocks with no determinism
    */
-  def factorProduct = (blockInfo: Gibbs.BlockInfo) => new SimpleBlockSampler(blockInfo) with Cached with FactorProduct
-
-  /**
-   * A combination of continuous and factorProduct
-   */
-  def continuousFactorProduct = (blockInfo: Gibbs.BlockInfo) => new SimpleBlockSampler(blockInfo) with Cached with GaussianWeight with FactorProduct
+  def joint = (blockInfo: Gibbs.BlockInfo) => new SimpleBlockSampler(blockInfo) with Cached
 }
 
 /**
  *  Class for handling sampling on a block
+ *  @param blockInfo The pair containing the variables to sample and adjacent factors.
  */
 abstract class BlockSampler(val blockInfo: Gibbs.BlockInfo) {
   val (block, adjacentFactors) = blockInfo
 
-  // Maps a variable to its index in the block
-  val indexMap = block.zipWithIndex.toMap
-
   /**
    * Get the factor from which to sample this block
+   * Returns a non-logarithmic factor
    */
-  def getSamplingFactor(currentSamples: Map[Variable[_], Int]): Factor[Double]
+  def getSamplingFactor(currentSamples: MutableMap[Variable[_], Int]): Factor[Double]
 
   /**
    * Sample this block once
    */
-  def sample(currentSamples: Map[Variable[_], Int]): Unit = {
+  def sample(currentSamples: MutableMap[Variable[_], Int]): Unit = {
     // Get the joint factor over variables in this block conditioned on the samples
     val samplingFactor = getSamplingFactor(currentSamples)
-    // Ensure the entries sum to 1 for sampling
-    normalizeFactor(samplingFactor)
     // Sample from the factor
-    val sample =
-      if(samplingFactor.isEmpty) sampleZeroProbability()
-      else sampleFactor(samplingFactor)
+    val sample = sampleFactor(samplingFactor)
     // Update currentSamples
     samplingFactor.variables.zip(sample).foreach(varAndSample => currentSamples(varAndSample._1) = varAndSample._2)
   }
 
   /**
    * Select a set of indices in the factor according to the weights in the factor
+   * Works on a non-logarithmic factor
    */
   def sampleFactor(factor: Factor[Double]): List[Int] = {
     // This implementation closely mirrors the sampleMultinomial method in figaro.util
@@ -87,32 +78,24 @@ abstract class BlockSampler(val blockInfo: Gibbs.BlockInfo) {
     }
     helper(random.nextDouble)
   }
-
-  /**
-   * Normalize a factor so its weights sum to 1
-   */
-  def normalizeFactor(factor: Factor[Double]): Unit = {
-    // Sum all of the entries and divide
-    val normalizingConstant = factor.foldLeft(0.0, _ + _)
-    factor.getIndices.foreach(index => factor.set(index, factor.get(index) / normalizingConstant))
-  }
-
-  /**
-   * Produce a sample when the Markov blanket indicates zero probability of all states in the block
-   * This implementation samples uniformly from the range of each variable in the block
-   */
-  def sampleZeroProbability(): List[Int] = block.map(v => random.nextInt(v.size))
 }
 
 class SimpleBlockSampler(blockInfo: Gibbs.BlockInfo)
   extends BlockSampler(blockInfo) {
 
+  // Work in log space to prevent underflow
+  val semiring = LogSumProductSemiring()
+
   // Indices over possible assignments to variables in the block
   val indices = new Indices(block)
 
-  def getSamplingFactor(currentSamples: Map[Variable[_], Int]): Factor[Double] = computeSamplingFactor(currentSamples)
+  // Maps a variable to its index in the block
+  val indexMap = block.zipWithIndex.toMap
 
-  def computeSamplingFactor(currentSamples: Map[Variable[_], Int]): Factor[Double] = {
+  // Override e.g. for caching
+  def getSamplingFactor(currentSamples: MutableMap[Variable[_], Int]): Factor[Double] = computeSamplingFactor(currentSamples)
+
+  def computeSamplingFactor(currentSamples: MutableMap[Variable[_], Int]): Factor[Double] = {
     // Sparse to make sampling faster when variables in the block are correlated
     val result = new SparseFactor[Double](block, List())
     // Loop through all possible assignments of variables in this block
@@ -123,18 +106,34 @@ class SimpleBlockSampler(blockInfo: Gibbs.BlockInfo)
         // Have to convert block index into an index in the factor
         val factorIndex = factor.variables.map(v => if(block.contains(v)) blockIndex(indexMap(v)) else currentSamples(v))
         factor.get(factorIndex)
-      }).product
-      if(prob > 0.0) result.set(blockIndex, prob)
+      }).foldLeft(semiring.one)(semiring.product)
+      if(prob != semiring.zero) result.set(blockIndex, prob)
     })
-    result
+    // Ensure the entries sum to 1 for sampling
+    normalizeFactor(result)
+  }
+
+  /**
+   * Normalize a factor so its weights sum to 1
+   * Takes a logarithmic factor and returns a non-logarithmic factor
+   */
+  def normalizeFactor(factor: Factor[Double]): Factor[Double] = {
+    val normalizingConstant = semiring.sumMany(factor.contents.values)
+    if(normalizingConstant == semiring.zero) throw new ZeroTotalUnnormalizedProbabilityException
+    factor.mapTo((d: Double) => math.exp(d - normalizingConstant), SumProductSemiring())
   }
 }
 
 /**
- * A VE-like procedure that may work well on large but highly sparse blocks.
+ * A VE-like procedure that may work well on large but highly sparse blocks. For each adjacent factor,
+ * this groups the rows of the factor into sub-factors according to possible Markov blanket assignments.
+ * Each of these sub-factors is accumulated into a factor over factors, which essentially maps the
+ * Markov blanket of the original factor to a conditional distribution over the block. This does not
+ * store any new information, but rather takes the information in each factor and stores it in an
+ * easier to use format.
  */
 trait FactorProduct extends SimpleBlockSampler {
-  lazy val subFactors = adjacentFactors.map(factor => {
+  lazy val mbLookupFactors = adjacentFactors.map(factor => {
     // Separate block and Markov blanket variables
     // Markov blanket consists of variables in this factor, but not in the block
     val (blockVars, mbVars) = factor.variables.partition(block.contains(_))
@@ -153,38 +152,59 @@ trait FactorProduct extends SimpleBlockSampler {
 
     mbMap.foreach(pair => {
       val (mbIndex, rows) = pair
-      val subResult = new SparseFactor[Double](blockVars, List())
+      val subResult = new SparseFactor[Double](blockVars, List(), semiring)
       // Fill the sub-factor with the possible rows in this block
       rows.foreach(row => {
         val (_, blockIndex, prob) = row
-        subResult.set(blockIndex, prob)
+        // No reason to keep the zero probability rows when factors are sparse
+        if(prob != semiring.zero) subResult.set(blockIndex, prob)
       })
       result.set(mbIndex, subResult)
     })
     result
   })
 
-  override def computeSamplingFactor(currentSamples: Map[Variable[_], Int]): Factor[Double] = {
+  lazy val ord = new Ordering[Factor[Double]] {
+    def compare(x: Factor[Double], y: Factor[Double]): Int = {
+      // Prioritize the factor with the fewest average entries per variable, i.e. most variables per entry
+      val xSize = math.pow(x.contents.size, 1.0 / x.numVars.toDouble)
+      val ySize = math.pow(y.contents.size, 1.0 / y.numVars.toDouble)
+      math.signum(ySize - xSize).toInt
+    }
+  }
+
+  override def computeSamplingFactor(currentSamples: MutableMap[Variable[_], Int]): Factor[Double] = {
     // Extract the rows from each factor and take their product
-    val toMultiply: List[Factor[Double]] = subFactors.map(factor => factor.get(factor.variables.map(currentSamples(_))))
-    toMultiply.reduce(_.product(_))
+    val toMultiply: List[Factor[Double]] = mbLookupFactors.map(factor => factor.get(factor.variables.map(currentSamples(_))))
+    /*
+     * This priority queue helps ensure that all intermediate products remain sparse. Even though we know
+     * the final product should be sparse, it's possible for intermediate factors to become exponentially
+     * large in the size of the block if multiplied in the wrong order
+     */
+    val queue = PriorityQueue(toMultiply:_*)(ord)
+    while(queue.length > 1) {
+      val product = queue.dequeue().product(queue.dequeue())
+      queue.enqueue(product)
+    }
+    normalizeFactor(queue.dequeue())
   }
 }
 
 /**
  * Caches factors according to assignments of values in the Markov blanket, which avoids recomputing the same factors repeatedly
+ * Takes advantage of the fact in Gibbs sampling, nearby samples tend to be highly correlated
  */
 trait Cached extends SimpleBlockSampler {
   lazy val markovBlanket = (adjacentFactors.flatMap(_.variables).toSet -- block.toSet).toList
 
   // Maps an assignment of values to the Markov blanket to a distribution over variables in the block
   // Implemented as a LinkedHashMap so calling cache.head returns the oldest entry
-  lazy val cache: Map[List[Int], Factor[Double]] = LinkedHashMap()
+  lazy val cache: MutableMap[List[Int], Factor[Double]] = LinkedHashMap()
 
   // Override to limit the cache size
-  lazy val maxSize = Int.MaxValue
+  lazy val maxSize = 1000
 
-  override def getSamplingFactor(currentSamples: Map[Variable[_], Int]): Factor[Double] = {
+  override def getSamplingFactor(currentSamples: MutableMap[Variable[_], Int]): Factor[Double] = {
     val key = markovBlanket.map(currentSamples(_))
     cache.get(key) match {
       // Return the cached factor
@@ -219,7 +239,7 @@ trait DoubleWeight extends SimpleBlockSampler {
         val newFactor = factor.createFactor(List(selector), List(result))
         factor.getIndices.foreach(index => {
           val factorProb = factor.get(index)
-          if(factorProb != 0.0) newFactor.set(index, factorProb)
+          if(factorProb != semiring.zero) newFactor.set(index, factorProb)
           else {
             val List(selectorIndex, resultIndex) = index
             val selectorExtended = selector.range(selectorIndex)
@@ -228,8 +248,8 @@ trait DoubleWeight extends SimpleBlockSampler {
             (selectorExtended, resultExtended) match {
               // This pattern matching basically just ensures that both the Chain and result are Doubles
               case (Regular((_, Regular(chainValue: Double))), Regular(resultValue: Double)) =>
-                newFactor.set(index, weight(chainValue, resultValue, selector.chain.asInstanceOf[Variable[Double]]))
-              case _ => newFactor.set(index, 0.0)
+                newFactor.set(index, logWeight(chainValue, resultValue, selector.chain.asInstanceOf[Variable[Double]]))
+              case _ => newFactor.set(index, semiring.zero)
             }
           }
         })
@@ -239,16 +259,16 @@ trait DoubleWeight extends SimpleBlockSampler {
     }
   })
 
-  // Function with which to assign probabilities in place of zeros
-  // It is assumed that if chainValue == resultValue, the result is 1.0
-  def weight(chainValue: Double, resultValue: Double, chain: Variable[Double]): Double
+  // Function with which to assign weights in place of zeros
+  // It is assumed that if chainValue == resultValue, the result is 0.0
+  def logWeight(chainValue: Double, resultValue: Double, chain: Variable[Double]): Double
 }
 
 /**
- * Assigns probabilities to continuous variables based on a Gaussian PDF
+ * Assigns weights to continuous variables based on a Gaussian PDF
  */
 trait GaussianWeight extends DoubleWeight {
-  lazy val varianceMap: Map[Variable[_], Double] = Map()
+  lazy val varianceMap: MutableMap[Variable[_], Double] = MutableMap()
 
   /*
    * The standard deviation is taken to be the difference between the Chain's minimum and maximum values, divided by the size of the Chain's support
@@ -256,7 +276,7 @@ trait GaussianWeight extends DoubleWeight {
    * then given a value from the variable's support, the next higher or lower value will be approximately 1 standard deviation away
    * This enables good mixing while also making it unlikely to sample when the resultValue and chainValue are far apart
    */
-  def weight(resultValue: Double, chainValue: Double, chain: Variable[Double]): Double = {
+  def logWeight(resultValue: Double, chainValue: Double, chain: Variable[Double]): Double = {
     // Cache the variance because iterating through the range every time is slow and unnecessary
     val variance = varianceMap.get(chain) match {
       case Some(variance) => variance
@@ -271,7 +291,7 @@ trait GaussianWeight extends DoubleWeight {
     }
     val diff = resultValue - chainValue
     // No need to multiply by a constant at the front because we normalize later anyways
-    // Moreover, we want this to return 1.0 when diff == 0.0
-    math.exp(- (diff * diff) / (2.0 * variance))
+    // Moreover, we want this to return 0.0 when diff == 0.0
+    - (diff * diff) / (2.0 * variance)
   }
 }
