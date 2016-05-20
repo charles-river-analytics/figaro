3f4120cf09bd36f0ba0cec3a3825da3bbe656d2a
Kenneth Lu <krlu91@gmail.com>
2016-05-20 12:51:37 -0400

    updates to ComponentCollection.scala

--- Figaro/src/main/scala/com/cra/figaro/algorithm/structured/ComponentCollection.scala
+++ Figaro/src/main/scala/com/cra/figaro/algorithm/structured/ComponentCollection.scala
@@ -17,7 +17,17 @@ import com.cra.figaro.language._
 import com.cra.figaro.library.collection.MakeArray
 import scala.collection.mutable.Map
 import com.cra.figaro.algorithm.factored.factors._
-
+import scala.collection.mutable.HashMap
+/**
+* To speed up factor creation time, it's necessary to override the hashcode of component collections.
+*/
+object ComponentHash {
+  var hashCodeState = 10
+  def nextCode: Int = {
+    hashCodeState += 1
+    hashCodeState
+  }
+}
 
 /**
  * A collection of problem components. This data structure manages all the components being used in the solution of a top-level
@@ -36,7 +46,9 @@ class ComponentCollection {
   val variableParents: Map[Variable[_], Set[Variable[_]]] = Map().withDefaultValue(Set())
 
   /** All the components in the collection, each associated with an element. */
-  val components: Map[Element[_], ProblemComponent[_]] = Map()
+  val components: Map[Element[_], ProblemComponent[_]] = new HashMap[Element[_], ProblemComponent[_]]() {
+    override val hashCode = ComponentHash.nextCode
+  }
 
   /**
    *  Intermediate variables defined during the construction of factors.
