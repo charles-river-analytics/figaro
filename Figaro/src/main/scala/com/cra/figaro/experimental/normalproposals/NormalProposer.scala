10722ffbf6d5e44b838a723626615c0c2328d9a7
William Kretschmer <william-kretschmer@comcast.net>
2016-08-25 13:36:46 -0400

    Better comments for NormalProposer
    Mostly explaining the subleties related to densities of randomness and
    value.

--- Figaro/src/main/scala/com/cra/figaro/experimental/normalproposals/NormalProposer.scala
+++ Figaro/src/main/scala/com/cra/figaro/experimental/normalproposals/NormalProposer.scala
@@ -27,7 +27,7 @@ trait NormalProposer extends Atomic[Double] {
   type Randomness = Double
 
   /**
-   * Inclusive lower bound of the range of the randomness of this element. Defaults to -Infinity. Must be strictly less
+   * Exclusive lower bound of the range of the randomness of this element. Defaults to -Infinity. Must be strictly less
    * than the upper bound.
    */
   def lower: Double = Double.NegativeInfinity
@@ -45,7 +45,7 @@ trait NormalProposer extends Atomic[Double] {
   def proposalScale: Double
 
   /**
-   * A strictly monotone differentiable function defined on [`lower`, `upper`). Defaults to the identity function.
+   * A strictly monotone differentiable function defined on (`lower`, `upper`). Defaults to the identity function.
    */
   def generateValue(rand: Randomness) = rand
 
@@ -67,12 +67,23 @@ trait NormalProposer extends Atomic[Double] {
    *
    * P(new) / P(old)
    *
-   * This implementation proposes a normally distributed randomness from the previous randomness, truncated to be within
-   * the range of this element. The probability ratios returned are associated with the values of this element rather
-   * than the randomness.
+   * By default, this implementation proposes a normally distributed randomness from the previous randomness, truncated
+   * to be within the appropriate range. The probability ratios returned are associated with the values of this element
+   * rather than the randomness. This is for the purpose of simulated annealing, since the most likely randomness is not
+   * necessarily the most likely value, depending on the form of the generateValue function.
+   *
+   * One can override this to only use normal proposals in certain special cases.
+   */
+  override def nextRandomness(oldRandomness: Randomness): (Randomness, Double, Double) = {
+    normalNextRandomness(oldRandomness)
+  }
+
+  /**
+   * Computes the normal proposal for nextRandomness. This is separated from the nextRandomness so that subclasses can
+   * choose when to use normal proposals.
    */
   @tailrec
-  override final def nextRandomness(oldRandomness: Randomness): (Randomness, Double, Double) = {
+  protected final def normalNextRandomness(oldRandomness: Randomness): (Randomness, Double, Double) = {
     /*
      * Sample from a normal distribution centered at oldRandomness, and reject if it's outside the range.
      * Rejection sampling to be in the range is justified here because of the assumptions on the scale.
@@ -84,28 +95,38 @@ trait NormalProposer extends Atomic[Double] {
      * So, the expected number of trials is never more than 1 / 0.4772 =  2.095, which is quite reasonable.
      */
     val newRandomness = oldRandomness + random.nextGaussian() * proposalScale
-    if(lower <= newRandomness && newRandomness < upper) {
+    if(lower < newRandomness && newRandomness < upper) {
       val proposalRatio = proposalProb(newRandomness, oldRandomness) / proposalProb(oldRandomness, newRandomness)
       val modelRatio = density(generateValue(newRandomness)) / density(generateValue(oldRandomness))
       (newRandomness, proposalRatio, modelRatio)
     }
-    else nextRandomness(oldRandomness)
+    else normalNextRandomness(oldRandomness)
   }
 
   /**
-   * Computes the proposal probability density in one direction. Both values should be in the interval [lower, upper).
+   * Computes the proposal probability density in one direction. Both values should be in the interval (lower, upper).
    * @param oldRandomness The previous randomness.
    * @param newRandomness The newly proposed randomness.
    * @return The probability density associated with the transition from `generateValue(oldRandomness)` to
    * `generateValue(newRandomness)`. This is a density over the corresponding values (as opposed to randomnesses).
    */
-  private def proposalProb(oldRandomness: Double, newRandomness: Double): Double = {
+  protected def proposalProb(oldRandomness: Double, newRandomness: Double): Double = {
     val dist = new NormalDistribution(oldRandomness, proposalScale)
-    // The probability density of proposing the new value given the old value is the density of the new value from the
-    // normal distribution, divided by the normalizing constant of the cumulative probability between the upper and
-    // lower bounds. This ensures that the PDF of the truncated distribution integrates to 1.
+
+    // The probability density of proposing the new randomness given the old randomness is the density of the new
+    // randomness from the normal distribution, divided by the normalizing constant of the cumulative probability
+    // between the upper and lower bounds. This cumulative probability corresponds to the probability of not rejecting
+    // when we sample from the regular normal proposal. This ensures that the PDF of the truncated distribution
+    // integrates to 1.
     val uncorrected = dist.density(newRandomness) / dist.probability(lower, upper)
-    // Correct for the scaling factor associated with newRandomness.
+
+    // Correct for the scaling factor associated with newRandomness. This is needed because even though the proposal
+    // distribution over the randomness of this element is a truncated normal distribution, the resulting proposal
+    // distribution over the actual values of this element may be different. Consider, for example, how an inverse Gamma
+    // element might use a Gamma distribution as its randomness, then invert the randomness to produce a value. Then, a
+    // truncated normal distribution over the randomness would have some other distribution over the values. Since we
+    // have the restriction that the deterministic transformation function is strictly monotone and differentiable, we
+    // can account for this difference; this is a standard "change of variable" computation.
     uncorrected / generateValueDerivative(newRandomness)
   }
 }
