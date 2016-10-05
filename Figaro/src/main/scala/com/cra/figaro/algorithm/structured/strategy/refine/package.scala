c1b5632876765a07e8f1e24c57a08e9de36e2b17
William Kretschmer <william-kretschmer@comcast.net>
2016-10-05 16:34:46 -0400

    Rename package object

---
+++ Figaro/src/main/scala/com/cra/figaro/algorithm/structured/strategy/refine/package.scala
@@ -0,0 +1,27 @@
+/*
+ * package.scala
+ * Definitions of expansion and solution strategies.
+ *
+ * Created By:      Avi Pfeffer (apfeffer@cra.com)
+ * Creation Date:   March 1, 2015
+ *
+ * Copyright 2015 Avrom J. Pfeffer and Charles River Analytics, Inc.
+ * See http://www.cra.com or email figaro@cra.com for information.
+ *
+ * See http://www.github.com/p2t2/figaro for a copy of the software license.
+ */
+package com.cra.figaro.algorithm.structured.strategy
+
+import com.cra.figaro.algorithm.factored.ParticleGenerator
+import com.cra.figaro.algorithm.structured.ProblemComponent
+
+
+package object refine {
+
+  /**
+   * A range sizer chooses a size of range for components corresponding to atomic elements.
+   */
+  type RangeSizer = ProblemComponent[_] => Int
+
+  def defaultRangeSizer(pc: ProblemComponent[_]) = ParticleGenerator.defaultTotalSamples
+}
