4abc04bfcec9962f2a94075471e740ab864b9b52
Glenn Takata <gtakata@cra.com>
2017-08-10 12:02:32 -0400

    Create a function for use with MakeList

--- FigaroExamples/src/main/scala/com/cra/figaro/example/MultiValuedReferenceUncertainty.scala
+++ FigaroExamples/src/main/scala/com/cra/figaro/example/MultiValuedReferenceUncertainty.scala
@@ -30,7 +30,7 @@ object MultiValuedReferenceUncertainty {
   val specialComponent1 = new Component
   val specialComponent2 = new Component
 
-  def makeComponent() = Select(0.1 -> specialComponent1, 0.2 -> specialComponent2, 0.7 -> new Component)
+  def makeComponent = () => Select(0.1 -> specialComponent1, 0.2 -> specialComponent2, 0.7 -> new Component)
 
   class Container extends ElementCollection {
     val components = MakeList(Select(0.5 -> 1, 0.5 -> 2), makeComponent)("components", this)
